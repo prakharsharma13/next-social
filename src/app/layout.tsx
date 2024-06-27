929b38f1a6d1bc1ca69ec2b937b1e9e7dc3191c5
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2024-06-27 22:46:32 +0530

    Responsive navbar with tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <div className="w-full bg-white px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          <Navbar />
+        </div>
+        <div className=" bg-slate-100 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+          {children}
+        </div>
       </body>
     </html>
   );
